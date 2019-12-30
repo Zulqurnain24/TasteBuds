21ce397d03e322b79366d797da9f10c1d25e0a0a
Mohammad Zulqurnain <mohammad.zulqurnain@gmail.com>
2019-12-30 13:21:02 +0500

    added navigation

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+ final String categoryID;
+ final String categoryTitle;
+
+ CategoryMealsScreen(this.categoryID, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: Text(
+          'The Recipes for the Category',
+        ),
+      ),
+    );
+  }
+}
